e61c6bb76a8161803ecd635d8c336ccf4fbbfbce
Piotr Stanny <piotr.stanny@gmail.com>
2020-09-03 00:21:46 +0100

    Add loading spinner on registration and logging in

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter_chat_app/components/rounded_button.dart';
 import 'package:flutter_chat_app/components/rounded_input.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_chat_app/screens/chat_screen.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 
 class LoginScreen extends StatefulWidget {
   static const String id = 'login';
@@ -13,6 +14,7 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen> {
   final _auth = FirebaseAuth.instance;
+  bool showSpinner = false;
   String email;
   String password;
 
@@ -20,69 +22,78 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Hero(
-              tag: 'logo',
-              child: Container(
-                height: 120.0,
-                child: Image.asset('images/logo.png'),
+      body: ModalProgressHUD(
+        inAsyncCall: showSpinner,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Hero(
+                tag: 'logo',
+                child: Container(
+                  height: 120.0,
+                  child: Image.asset('images/logo.png'),
+                ),
               ),
-            ),
-            SizedBox(
-              height: 48.0,
-            ),
-            RoundedInput(
-              borderColour: Colors.lightBlueAccent,
-              hintText: 'Enter your email',
-              inputType: TextInputType.emailAddress,
-              obscureText: false,
-              onChanged: (value) {
-                email = value;
-              },
-            ),
-            SizedBox(
-              height: 8.0,
-            ),
-            RoundedInput(
-              borderColour: Colors.lightBlueAccent,
-              hintText: 'Enter your password',
-              inputType: TextInputType.text,
-              obscureText: true,
-              onChanged: (value) {
-                password = value;
-              },
-            ),
-            SizedBox(
-              height: 24.0,
-            ),
-            RoundedButton(
-              title: 'Login',
-              colour: Colors.lightBlueAccent,
-              onPressed: () async {
-                try {
-                  UserCredential userCredential =
-                      await _auth.signInWithEmailAndPassword(
-                    email: email,
-                    password: password,
-                  );
-                  if (userCredential != null) {
-                    Navigator.pushNamed(context, ChatScreen.id);
+              SizedBox(
+                height: 48.0,
+              ),
+              RoundedInput(
+                borderColour: Colors.lightBlueAccent,
+                hintText: 'Enter your email',
+                inputType: TextInputType.emailAddress,
+                obscureText: false,
+                onChanged: (value) {
+                  email = value;
+                },
+              ),
+              SizedBox(
+                height: 8.0,
+              ),
+              RoundedInput(
+                borderColour: Colors.lightBlueAccent,
+                hintText: 'Enter your password',
+                inputType: TextInputType.text,
+                obscureText: true,
+                onChanged: (value) {
+                  password = value;
+                },
+              ),
+              SizedBox(
+                height: 24.0,
+              ),
+              RoundedButton(
+                title: 'Login',
+                colour: Colors.lightBlueAccent,
+                onPressed: () async {
+                  setState(() {
+                    showSpinner = true;
+                  });
+                  try {
+                    UserCredential userCredential =
+                        await _auth.signInWithEmailAndPassword(
+                      email: email,
+                      password: password,
+                    );
+                    if (userCredential != null) {
+                      Navigator.pushNamed(context, ChatScreen.id);
+                    }
+                    setState(() {
+                      showSpinner = false;
+                    });
+                  } on FirebaseAuthException catch (e) {
+                    if (e.code == 'user-not-found') {
+                      print('No user found for that email.');
+                    } else if (e.code == 'wrong-password') {
+                      print('Wrong password provided for that user.');
+                    }
                   }
-                } on FirebaseAuthException catch (e) {
-                  if (e.code == 'user-not-found') {
-                    print('No user found for that email.');
-                  } else if (e.code == 'wrong-password') {
-                    print('Wrong password provided for that user.');
-                  }
-                }
-              },
-            ),
-          ],
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
